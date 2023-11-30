1a81cc3ec505233077b10c343aa25c70aa5d2bb5
Marvin-Mawira <marvinmawira8@.com>
2023-11-30 15:55:07 +0300

    First commit

--- webdev/urls.py
+++ webdev/urls.py
@@ -29,6 +29,9 @@ urlpatterns = [
 ]
 
 
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 # from django.contrib import admin
 # from django.urls import path, include
 
